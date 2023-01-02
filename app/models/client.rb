f4a0f6c7e299ce12baea63aa6be624daff2cf6e6
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2023-01-02 11:12:36 +0300

    AJAX configuration

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,5 @@
 class Client < ApplicationRecord
-    
+    # has_many :comments
+    # has_many :reviews
+    # belongs_to :Property through :comments
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,4 @@
 class Property < ApplicationRecord
+    # has_many :comments
+    # has_many :clients, through: :comments
 end
