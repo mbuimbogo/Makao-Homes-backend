04383f17a995577655a9b8d1a906bfe524cba174
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2023-01-04 01:13:51 +0300

    Add get, create and delete property

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :clients
-  resources :properties
+  resources :properties, only: [:index, :show, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
