b7a73b19ae9c80e75716ee76a073ecbe6aa60346
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2023-01-04 15:38:41 +0300

    Add login and logout actions

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,5 +3,6 @@ class Client < ApplicationRecord
     has_many :reviews
     validates :name, presence: true
     validates :password, length: { in: 6..20 }
+    has_secure_password
     # belongs_to :Property through :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :clients
   resources :properties, only: [:index, :show, :create, :destroy]
+  post "/login", to: "sessions#create"
+  get "/me", to: "sessions#show"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
