6556b8ccfb4ae9ce25080057f5115c02cff69d48
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2023-01-05 14:21:05 +0300

    Add column username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_03_074042) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_094020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_03_074042) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
   create_table "properties", force: :cascade do |t|
