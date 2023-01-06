55b245cb6f610d7be3498dcc782d7594d3d0b8f9
tenmark86e <tenmark86@gmail.com>
2023-01-06 21:39:35 +0300

    created comments controller

---
+++ app/controllers/comment_controller.rb
@@ -0,0 +1,29 @@
+class CommentController < ApplicationController
+    before_action :logged_in_user, only: [:create, :destroy, :show]
+    before_action :correct_user,   only: :destroy
+
+def show
+  @comment = Comment.find(params[:id])
+end
+
+def create
+    @comment = Comment.create!(comment_params)
+    render json: comment, status: :created
+end
+
+def destroy
+    @comment.destroy
+    render json: comment, status: :destroy
+end
+
+
+private
+ def comment_params
+    params.permit(:user_id,:comment)
+ end
+ def correct_user
+   @comment = current_user.comments.find_by(id: params[:id])
+   redirect_to home_page if @comment.nil?
+ end
+end
+
