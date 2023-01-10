f3785a1e970430080555ef57d825bf665f124e54
Sheila <rugut.cheruto@student.moringaschool.com>
2023-01-10 16:27:04 +0300

    create comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_10_120935) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_10_125459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_10_120935) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "client_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "properties", force: :cascade do |t|
