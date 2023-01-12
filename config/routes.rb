cdd80001a326cbd8275001f7e1b85896b92d614d
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2023-01-12 15:55:20 +0300

    Add mpesa routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :mpesas
+  post "/stkpush", to: "mpesas#stkpush"
+  post "/polling_payment", to: "mpesas#polling_payment"
+  
   resources :comments, only: [:index, :create, :show]
   resources :reviews, only: [:index, :create, :show]
   resources :clients
